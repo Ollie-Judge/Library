cab6c40577e7b14d7009af589022d90b27a73442
Ollie-Judge <ollie.judge@hotmail.com>
2023-11-27 15:45:06 +0000

    got the output

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const form = document.getElementById("bookForm");
+const outputDiv = document.getElementById("outputContainer");
+
+form.addEventListener("submit", createLibraryItem);
+
+function createLibraryItem(e) {
+  e.preventDefault();
+  const myFormData = new FormData(e.target);
+  console.log(myFormData);
+
+  const formDataObj = {};
+  myFormData.forEach((value, key) => (formDataObj[key] = value));
+  console.log(formDataObj);
+
+  let newEntry = document.createElement("div");
+  newEntry.className = "newEntry";
+  newEntry.innerText = formDataObj.name;
+  outputDiv.appendChild(newEntry);
+}
+
 const myLibrary = [];
 
 function Book(name, author, pages) {
