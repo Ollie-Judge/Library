48f3ba0fa207a6b3f19c442578a315b3ddd97071
Ollie-Judge <ollie.judge@hotmail.com>
2023-11-27 17:07:48 +0000

    not getting the correct response getting empty inputs

--- script.js
+++ script.js
@@ -1,36 +1,22 @@
 const form = document.getElementById("bookForm");
-const outputDiv = document.getElementById("outputContainer");
 
-form.addEventListener("submit", createLibraryItem);
+let name = document.getElementById("name").value;
+let author = document.getElementById("author").value;
+let pages = document.getElementById("pages").value;
+let read = document.getElementById("read").value;
 
-function createLibraryItem(e) {
-  e.preventDefault();
-  const myFormData = new FormData(e.target);
-  console.log(myFormData);
+let myLibrary = [];
 
-  const formDataObj = {};
-  myFormData.forEach((value, key) => (formDataObj[key] = value));
-  console.log(formDataObj);
-
-  let newEntry = document.createElement("div");
-  newEntry.className = "newEntry";
-  newEntry.innerText = formDataObj.name;
-  outputDiv.appendChild(newEntry);
-}
-
-const myLibrary = [];
-
-function Book(name, author, pages) {
+function Book(name, author, pages, read) {
   this.name = name;
   this.author = author;
   this.pages = pages;
+  this.read = read;
 }
 
-function addBookToLibrary(variable) {
-  myLibrary.push(variable);
+form.onsubmit = function addBookToLibrary(e) {
+  e.preventDefault();
+
+  myLibrary.push(new Book(name, this.author, this.pages, this.read));
   console.log(myLibrary);
-}
-
-const book1 = new Book("book1", "qwrty", 103);
-
-addBookToLibrary(book1);
+};
