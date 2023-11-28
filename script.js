d716dc4333f394d26caaae3c0ec06ae3f2cbb60f
Ollie-Judge <ollie.judge@hotmail.com>
2023-11-28 17:55:25 +0000

    put the outputs into seperate divs

--- script.js
+++ script.js
@@ -5,7 +5,26 @@ let newBookAuthor = document.getElementById("newBookAuthorContainer");
 let newBookPages = document.getElementById("newBookPagesContainer");
 let newBookRead = document.getElementById("newBookReadContainer");
 
-let myLibrary = [];
+let myLibrary = [
+  {
+    name: "Lord of The Rings: The Fellowship of The Ring",
+    author: "J.R.R Tolkein",
+    pages: 432,
+    read: true,
+  },
+  {
+    name: "Lord of The Rings: The Two Towers",
+    author: "J.R.R Tolkein",
+    pages: 448,
+    read: true,
+  },
+  {
+    name: "Lord of The Rings: The Return of The King",
+    author: "J.R.R Tolkein",
+    pages: 432,
+    read: true,
+  },
+];
 
 function Book(name, author, pages, read) {
   this.name = name;
@@ -29,28 +48,33 @@ form.onsubmit = function addBookToLibrary(e) {
 
 let outputOfUserInputBooks = () => {
   myLibrary.forEach((book) => {
-    const h5 = document.createElement("h5");
-    const para1 = document.createElement("p");
-    const para2 = document.createElement("p");
-    const checkBox = document.createElement("input");
-    checkBox.setAttribute("type", "checkbox");
+    const outputContainer = document.getElementById("outputContainer");
 
+    const bookContainer = document.createElement("div");
+    outputContainer.appendChild(bookContainer);
+    bookContainer.classList = "bookContainer";
+
+    const h5 = document.createElement("h5");
     const bookTitle = document.createTextNode(book.name);
     h5.appendChild(bookTitle);
+    bookContainer.appendChild(h5);
 
+    const para1 = document.createElement("p");
     const bookAuthor = document.createTextNode(book.author);
     para1.appendChild(bookAuthor);
+    bookContainer.appendChild(para1);
 
+    const para2 = document.createElement("p");
     const bookPages = document.createTextNode(book.pages);
     para2.appendChild(bookPages);
+    bookContainer.appendChild(para2);
 
+    const checkBox = document.createElement("input");
+    checkBox.setAttribute("type", "checkbox");
     checkBox.setAttribute("type", "checkbox");
     checkBox.checked = book.read;
+    bookContainer.appendChild(checkBox);
 
-    document.getElementById("newBookNameContainer").appendChild(h5);
-    document.getElementById("newBookAuthorContainer").appendChild(para1);
-    document.getElementById("newBookPagesContainer").appendChild(para2);
-    document.getElementById("newBookReadContainer").appendChild(checkBox);
     console.log("book", book, book.name);
   });
 };
