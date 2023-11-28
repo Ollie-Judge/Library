b0db12fb6d7c4b6f4d61c2a92693ecdc4a37b3f9
Ollie-Judge <ollie.judge@hotmail.com>
2023-11-28 16:03:44 +0000

    created a new book using only inputs from user

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const form = document.getElementById("bookForm");
 
+let newBookName = document.getElementById("newBookNameContainer");
+let newBookAuthor = document.getElementById("newBookAuthorContainer");
+let newBookPages = document.getElementById("newBookPagesContainer");
+let newBookRead = document.getElementById("newBookReadContainer");
+
 let myLibrary = [];
 
 function Book(name, author, pages, read) {
@@ -19,4 +24,33 @@ form.onsubmit = function addBookToLibrary(e) {
 
   myLibrary.push(new Book(name, author, pages, read));
   console.log(myLibrary);
+  outputOfUserInputBooks();
+};
+
+let outputOfUserInputBooks = () => {
+  myLibrary.forEach((book) => {
+    const h5 = document.createElement("h5");
+    const para1 = document.createElement("p");
+    const para2 = document.createElement("p");
+    const checkBox = document.createElement("input");
+    checkBox.setAttribute("type", "checkbox");
+
+    const bookTitle = document.createTextNode(book.name);
+    h5.appendChild(bookTitle);
+
+    const bookAuthor = document.createTextNode(book.author);
+    para1.appendChild(bookAuthor);
+
+    const bookPages = document.createTextNode(book.pages);
+    para2.appendChild(bookPages);
+
+    checkBox.setAttribute("type", "checkbox");
+    checkBox.checked = book.read;
+
+    document.getElementById("newBookNameContainer").appendChild(h5);
+    document.getElementById("newBookAuthorContainer").appendChild(para1);
+    document.getElementById("newBookPagesContainer").appendChild(para2);
+    document.getElementById("newBookReadContainer").appendChild(checkBox);
+    console.log("book", book, book.name);
+  });
 };
