ccefe74f29b6292b760879f6a63e3969394c04d2
Ollie-Judge <ollie.judge@hotmail.com>
2023-11-29 18:19:46 +0000

    solved duplication issues

--- script.js
+++ script.js
@@ -43,38 +43,71 @@ form.onsubmit = function addBookToLibrary(e) {
 
   myLibrary.push(new Book(name, author, pages, read));
   console.log(myLibrary);
-  outputOfUserInputBooks();
+  displayItemAdded();
 };
 
-let outputOfUserInputBooks = () => {
-  myLibrary.forEach((book) => {
-    const outputContainer = document.getElementById("outputContainer");
+myLibrary.forEach((book) => {
+  const outputContainer = document.getElementById("outputContainer");
 
-    const bookContainer = document.createElement("div");
-    outputContainer.appendChild(bookContainer);
-    bookContainer.classList = "bookContainer";
+  const bookContainer = document.createElement("div");
+  outputContainer.appendChild(bookContainer);
+  bookContainer.classList = "bookContainer";
 
-    const h5 = document.createElement("h5");
-    const bookTitle = document.createTextNode(book.name);
-    h5.appendChild(bookTitle);
-    bookContainer.appendChild(h5);
+  const h5 = document.createElement("h5");
+  const bookTitle = document.createTextNode(book.name);
+  h5.appendChild(bookTitle);
+  bookContainer.appendChild(h5);
 
-    const para1 = document.createElement("p");
-    const bookAuthor = document.createTextNode(book.author);
-    para1.appendChild(bookAuthor);
-    bookContainer.appendChild(para1);
+  const para1 = document.createElement("p");
+  const bookAuthor = document.createTextNode(book.author);
+  para1.appendChild(bookAuthor);
+  bookContainer.appendChild(para1);
 
-    const para2 = document.createElement("p");
-    const bookPages = document.createTextNode(book.pages);
-    para2.appendChild(bookPages);
-    bookContainer.appendChild(para2);
+  const para2 = document.createElement("p");
+  const bookPages = document.createTextNode(book.pages);
+  para2.appendChild(bookPages);
+  bookContainer.appendChild(para2);
 
-    const checkBox = document.createElement("input");
-    checkBox.setAttribute("type", "checkbox");
-    checkBox.setAttribute("type", "checkbox");
-    checkBox.checked = book.read;
-    bookContainer.appendChild(checkBox);
+  const checkBox = document.createElement("input");
+  checkBox.setAttribute("type", "checkbox");
+  checkBox.setAttribute("type", "checkbox");
+  checkBox.checked = book.read;
+  bookContainer.appendChild(checkBox);
 
-    console.log("book", book, book.name);
-  });
+  console.log("book", book, book.name);
+});
+
+const displayItemAdded = () => {
+  const outputContainer = document.getElementById("outputContainer");
+
+  const bookContainer = document.createElement("div");
+  outputContainer.appendChild(bookContainer);
+  bookContainer.classList = "bookContainer";
+
+  const h5 = document.createElement("h5");
+  const bookTitle = document.createTextNode(
+    myLibrary[myLibrary.length - 1].name
+  );
+  h5.appendChild(bookTitle);
+  bookContainer.appendChild(h5);
+
+  const para1 = document.createElement("p");
+  const bookAuthor = document.createTextNode(
+    myLibrary[myLibrary.length - 1].author
+  );
+  para1.appendChild(bookAuthor);
+  bookContainer.appendChild(para1);
+
+  const para2 = document.createElement("p");
+  const bookPages = document.createTextNode(
+    myLibrary[myLibrary.length - 1].pages
+  );
+  para2.appendChild(bookPages);
+  bookContainer.appendChild(para2);
+
+  const checkBox = document.createElement("input");
+  checkBox.setAttribute("type", "checkbox");
+  checkBox.setAttribute("type", "checkbox");
+  checkBox.checked = myLibrary[myLibrary.length - 1].read;
+  bookContainer.appendChild(checkBox);
 };
